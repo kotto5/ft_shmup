e593857a8ee05d53d29a519a72a9248501e1d13c
dorakueYaritai <kotto555555@gmail.com>
2024-06-15 16:15:18 +0900

    collide

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -39,6 +39,30 @@ Coordinate update(Coordinate player, std::vector<Coordinate> enemies, int ch) {
   return player;
 }
 
+bool collide(Coordinate player, std::vector<Coordinate> enemies) {
+	for (size_t i = 0; i < enemies.size(); i++) {
+		if (player.x == enemies[i].x && player.y == enemies[i].y) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int	game_over() {
+	clear();
+	mvprintw(10, 10, "Game Over\n If you want to quit press 'q'. If you want to play again press 'n'.");
+	refresh();
+	while (1) {
+		int ch = getch();
+		if (ch == 'q') {
+			endwin();
+			return 0;
+		}
+		else if (ch == 'n')
+			return main();
+	}
+}
+
 int main(void) {
   Coordinate player(0, 0);
   std::vector<Coordinate> enemies;
@@ -51,6 +75,8 @@ int main(void) {
       break;
     }
     player = update(player, enemies, ch);
+	if (collide(player, enemies))
+		return game_over();
   }
   endwin();
 }
