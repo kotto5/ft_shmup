afce94f7e5bf2005010e12dfb14ee56550473f75
kotto5 <kotto555555@gmail.com>
2024-06-16 21:23:29 +0900

    Player の移動の処理訂正

--- srcs/Player.hpp
+++ srcs/Player.hpp
@@ -12,27 +12,28 @@ public:
 	std::vector<Object *> change_status_and_produce_objects(int ch, int t) {
 		int width, height;
 		getmaxyx(stdscr, height, width);
-		int left_bound = t / frequency + 1;
-		int right_bound = t / frequency + width - 3;
-		if (this->x < left_bound) { // border barriers
-			this->x = left_bound;
+		int left_bound = t + 1;
+		int right_bound = left_bound + width;
+		Coordinate coo = get_coordinate(t);
+		if (coo.x == left_bound) { // border barriers
+			this->x++;
 		}
 		std::vector<Object *> objects;
 		if (ch == 'w') {
-			this->y == 1 ? this->y = 1 : this->y--;	
+			coo.y == 1 ? this->y = 1 : this->y--;	
 		}
 		else if (ch == 's') {
-			this->y == height - 3 ? this->y = height - 3 : this->y++;
+			coo.y == height - 3 ? this->y = height - 3 : this->y++;
 		}
 		else if (ch == 'a') {
-			this->x < left_bound ? this->x == left_bound : this->x--;
+			coo.x == left_bound ? this->x = left_bound : this->x--;
 		}
 		else if (ch == 'd') {
-			this->x > right_bound  ? this->x = right_bound : this->x++;
+			this->x = (coo.x == right_bound ? right_bound - 1 : this->x + 1);
 		}
 		else if (ch == ' ') {
 			Coordinate bullet_spawn = this->get_coordinate(t);
-			objects.push_back(new Bullet(bullet_spawn.x, bullet_spawn.y, t, [](int t) { return Coordinate(t, 0); }, BULLET_SYMBOL, 1));
+			objects.push_back(new Bullet(bullet_spawn.x, bullet_spawn.y, t, [](int t) { return Coordinate(t * 3, 0); }, BULLET_SYMBOL, 1));
 		}
 		return objects;
 	}
